87ee6facc95ebefb92f473f964a3d7c1feec0c88
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 11:53:34 -0500

    Ejercicio8 Java

---
+++ Ejercicio8Java/src/CicloJava.java
@@ -0,0 +1,33 @@
+/* 
+ * Escribir un programa ue pida 2 numeros por comnsola
+de manera reiterada.
+la ejecucion del programa terminar'a cuando los numeros 
+ingresados sean iguales.
+ 
+ */
+
+
+public class CicloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Ciclo do while
+		 * int num 1 = 0;
+		 * int num 2 = 0;
+		 * 
+		 * while(num1 != num 2) {
+		 * codigo
+		 * codigo
+		 * }
+		 * 
+		 * do {
+		 * codigo
+		 * codigo
+		 * codigo
+		 * } while(condición)
+		 */
+
+	}
+
+}
